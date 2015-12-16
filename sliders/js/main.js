a49a5d6609d302a444cfb370c97e0b1f077dc5e1
Ionut Firatoiu <firatoiu.ionut@gmail.com>
2015-12-16 12:09:37 +0200

    slidersColor2

--- sliders/js/main.js
+++ sliders/js/main.js
@@ -5,19 +5,22 @@ red = r.value;
 green = g.value;
 blue = b.value;
 
+function changeColor(event) {
+	document.body.style.backgroundColor = "rgb(" + red + ", " + green +"," + blue + ")";
+	console.log(red, green, blue);
+};
+
 r.addEventListener('change', function() {
 	red = r.value;
-})
+	changeColor();
+});
 
 g.addEventListener('change', function() {
 	green = g.value;
-})
+	changeColor();
+});
 
 b.addEventListener('change', function() {
 	blue = b.value;
-})
-
-document.addEventListener('change', function(event) {
-	document.body.style.backgroundColor = "rgb(" + red + ", " + green +"," + blue + ")";
-	console.log(red, green, blue);
-})
+	changeColor();
+});
